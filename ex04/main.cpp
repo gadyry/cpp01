60b63310ac211a8914ede7542beabb81484114e9
Abdelhak El Gadyry <ael-gady@e3r4p12.1337.ma>
2025-08-01 23:51:09 +0100

    ex04 is loading

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,11 +6,19 @@
 /*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 12:28:20 by ael-gady          #+#    #+#             */
-/*   Updated: 2025/08/01 23:47:24 by ael-gady         ###   ########.fr       */
+/*   Updated: 2025/08/01 23:51:00 by ael-gady         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+
+void	init_args(char **av, std::string& filename,
+		 std::string& s1, std::string& s2)
+{
+	filename = av[1];
+	s1 = av[2];
+	s2 = av[3];
+}
 
 int main(int ac, char **av)
 {
@@ -22,9 +30,7 @@ int main(int ac, char **av)
 		std::cout << "Try to tap : ./replace <filename> <s1> <s2>"<< std::endl;
 		return (1);
 	}
-	filename = av[1];
-	s1 = av[2];
-	s2 = av[3];
+	init_args(av, filename, s1, s2);
 	std::cout << filename << " | " << s1  << " | " << s2 << " | " << std::endl;
 	return (0);
 }
