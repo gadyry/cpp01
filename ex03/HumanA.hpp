8d60dc0e982c9692aaf95b9e75b8dee3ef2351c6
Abdelhak El Gadyry <ael-gady@e3r11p8.1337.ma>
2025-08-01 09:27:08 +0100

    main is loading

--- ex03/HumanA.hpp
+++ ex03/HumanA.hpp
@@ -6,7 +6,7 @@
 /*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 05:21:12 by ael-gady          #+#    #+#             */
-/*   Updated: 2025/08/01 09:10:16 by ael-gady         ###   ########.fr       */
+/*   Updated: 2025/08/01 09:15:25 by ael-gady         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ class HumanA
 {
 private :
 	Weapon& weapon;
-	std::string& name;
+	std::string name;
 public :
 	HumanA(const std::string& n, Weapon& w);
 	~HumanA();
