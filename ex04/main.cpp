a250a9331afd09efc4bda4f8b442acaae8689e77
Abdelhak El Gadyry <ael-gady@e3r4p14.1337.ma>
2025-08-02 05:20:42 +0100

    read from filename all content

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,12 +6,13 @@
 /*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 12:28:20 by ael-gady          #+#    #+#             */
-/*   Updated: 2025/08/01 23:59:27 by ael-gady         ###   ########.fr       */
+/*   Updated: 2025/08/02 05:20:15 by ael-gady         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 void	init_args(char **av, std::string& filename,
 		 std::string& s1, std::string& s2)
@@ -24,20 +25,30 @@ void	init_args(char **av, std::string& filename,
 int main(int ac, char **av)
 {
 	std::string filename;
-	std::string s1, s2;
+	std::string s1, s2, input;
 
-	if (ac > 4 || ac < 4)
+	if (ac != 4)
 	{
 		std::cerr << "Try to tap : ./replace <filename> <s1> <s2>."<< std::endl;
 		return (1);
 	}
 	init_args(av, filename, s1, s2);
+
+	// std::ofstream out(filename);
+	// out << "create a filename  :" << filename << std::endl;
+
 	std::ifstream in(filename);
 	if (!in.is_open())
 	{
 		std::cerr << "Error: could not open input file '" << filename << "' for reading."<< std::endl;
 		return (1);
 	}
-	
+	while (69)
+	{
+		getline(in, input);
+		if (input.empty())
+			break;
+		std::cout <<"this is a content of  "<<filename << " : " << input << std::endl;
+	}
 	return (0);
 }
