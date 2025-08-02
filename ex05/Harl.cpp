4e3a49d90802d3b08df9582e2705e6581a221473
Abdelhak El Gadyry <ael-gady@e3r7p13.1337.ma>
2025-08-02 07:04:57 +0100

    ex05 is loading

---
+++ ex05/Harl.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/02 05:45:11 by ael-gady          #+#    #+#             */
+/*   Updated: 2025/08/02 06:45:18 by ael-gady         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void	Harl::debug()
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+}
+
+void	Harl::info()
+{
+	std::cout<<"I cannot believe adding extra bacon costs more money. You didn’t put"
+		"enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+}
+
+void	Harl::warning()
+{
+	std::cout<<"I think I deserve to have some extra bacon for free. I’ve been coming for"
+		"years, whereas you started working here just last month.\n";
+}
+
+void	Harl::error()
+{
+	std::cout<<"This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void 	Harl::complain(std::string level)
+{
+	if (level == "DEBUG")
+		debug();
+	else if (level == "INFO")
+		info();
+	else if (level == "WARNING")
+		warning();
+	else if (level == "ERROR")
+		error();
+}
---
+++ ex05/Harl.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-gady <ael-gady@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/02 05:32:21 by ael-gady          #+#    #+#             */
+/*   Updated: 2025/08/02 05:39:10 by ael-gady         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+private :
+	void debug( void );
+	void info( void );
+	void warning( void );
+	void error( void );
+public :
+	void complain( std::string level );
+};
+
+#endif
