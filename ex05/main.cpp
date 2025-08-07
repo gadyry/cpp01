bfc1e0105eceff8662e11494b24d3e255df6bf4a
Abdelhak El Gadyry <87965749+gadyry@users.noreply.github.com>
2025-08-07 19:41:04 +0000

    ex06 is done git add .

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -12,24 +12,24 @@
 
 #include "Harl.hpp"
 
-int main() {
-    Harl harl;
+int main()
+{
+	Harl harl;
 
-    // Test each log level
-    std::cout << "== DEBUG ==" << std::endl;
-    harl.complain("DEBUG");
+	std::cout << "== DEBUG ==" << std::endl;
+	harl.complain("DEBUG");
 
-    std::cout << "\n== INFO ==" << std::endl;
-    harl.complain("INFO");
+	std::cout << "\n== INFO ==" << std::endl;
+	harl.complain("INFO");
 
-    std::cout << "\n== WARNING ==" << std::endl;
-    harl.complain("WARNING");
+	std::cout << "\n== WARNING ==" << std::endl;
+	harl.complain("WARNING");
 
-    std::cout << "\n== ERROR ==" << std::endl;
-    harl.complain("ERROR");
+	std::cout << "\n== ERROR ==" << std::endl;
+	harl.complain("ERROR");
 
-    std::cout << "\n== UNKNOWN ==" << std::endl;
-    harl.complain("NOTHING");
+	std::cout << "\n== UNKNOWN ==" << std::endl;
+	harl.complain("NOTHING");
 
-    return (0);
+	return (0);
 }
---
+++ ex06/main.cpp
@@ -0,0 +1,45 @@
+#include "Harl.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Usage: ./harl_filter <level>"<< std::endl;
+		return (1);
+	}
+
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	std::string input = argv[1];
+	Harl harl;
+	size_t index = -1377;
+
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (input == levels[i])
+		{
+			index = i;
+			break;
+		}
+	}
+
+	switch (index)
+	{
+		case 0:
+			std::cout << "[ DEBUG ]\n";
+			harl.complain("DEBUG");
+		case 1:
+			std::cout << "\n[ INFO ]\n";
+			harl.complain("INFO");
+		case 2:
+			std::cout << "\n[ WARNING ]\n";
+			harl.complain("WARNING");
+		case 3:
+			std::cout << "\n[ ERROR ]\n";
+			harl.complain("ERROR");
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]\n";
+	}
+
+	return (0);
+}
