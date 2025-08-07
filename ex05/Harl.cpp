6ac6518a64015a7b232e2c63d40a6c04bb17040e
Abdelhak El Gadyry <87965749+gadyry@users.noreply.github.com>
2025-08-07 18:55:07 +0000

    another version with pointer to member function

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -19,14 +19,14 @@ void	Harl::debug()
 
 void	Harl::info()
 {
-	std::cout<<"I cannot believe adding extra bacon costs more money. You didn’t put"
-		"enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+	std::cout<<"I cannot believe adding extra bacon costs more money."
+		 	<< " You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
 }
 
 void	Harl::warning()
 {
 	std::cout<<"I think I deserve to have some extra bacon for free. I’ve been coming for"
-		"years, whereas you started working here just last month.\n";
+			<< "years, whereas you started working here just last month.\n";
 }
 
 void	Harl::error()
@@ -34,14 +34,30 @@ void	Harl::error()
 	std::cout<<"This is unacceptable! I want to speak to the manager now.\n";
 }
 
+// void 	Harl::complain(std::string level)
+// {
+// 	if (level == "DEBUG")
+// 		debug();
+// 	else if (level == "INFO")
+// 		info();
+// 	else if (level == "WARNING")
+// 		warning();
+// 	else if (level == "ERROR")
+// 		error();
+// }
+
 void 	Harl::complain(std::string level)
 {
-	if (level == "DEBUG")
-		debug();
-	else if (level == "INFO")
-		info();
-	else if (level == "WARNING")
-		warning();
-	else if (level == "ERROR")
-		error();
+	typedef void (Harl::*HarlFnct)();
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	HarlFnct funct[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	for(size_t i = 0; i < 4; i++)
+	{
+		if (level == levels[i])
+		{
+			(this->funct[i])();
+			return ;
+		}
+	}
 }
